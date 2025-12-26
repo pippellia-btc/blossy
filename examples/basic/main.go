4598f590616b93711eae517d1702f94f0d08ebfa
pippellia <pippellia@gmail.com>
2025-12-26 18:00:48 +0100

    added basic example

---
+++ examples/basic/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"context"
+	"io"
+	"log/slog"
+	"os"
+	"os/signal"
+
+	blossy "github.com/pippellia-btc/bloomy"
+	"github.com/pippellia-btc/blossom"
+)
+
+func main() {
+	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt, os.Kill)
+	defer cancel()
+
+	blossom, err := blossy.NewServer()
+	if err != nil {
+		panic(err)
+	}
+
+	blossom.On.FetchBlob = BlobNotFound
+	blossom.On.FetchMeta = MetaNotFound
+
+	err = blossom.StartAndServe(ctx, "localhost:3335")
+	if err != nil {
+		panic(err)
+	}
+}
+
+func BlobNotFound(r blossy.Request, hash blossom.Hash, ext string) (io.ReadSeekCloser, *blossom.Error) {
+	slog.Info("received GET request", "hash", hash, "ext", ext, "ip", r.IP().Group())
+	return nil, &blossom.Error{Code: 404, Reason: "Blob not found"}
+}
+
+func MetaNotFound(r blossy.Request, hash blossom.Hash, ext string) (string, int64, *blossom.Error) {
+	slog.Info("received HEAD request", "hash", hash, "ext", ext, "ip", r.IP().Group())
+	return "", 0, &blossom.Error{Code: 404, Reason: "Blob not found"}
+}
